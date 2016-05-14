ff5f30c28c8f6035359b676f1992012b17a13f44
Daniel Castro García <dandev237@gmail.com>
2016-05-14 17:55:07 +0200

    Primera versión funcional

--- app/src/main/java/dandev237/wifitest/MainActivity.java
+++ app/src/main/java/dandev237/wifitest/MainActivity.java
@@ -1,13 +1,74 @@
 package dandev237.wifitest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    WifiManager wifiManager;
+    WifiScanReceiver wifiReceiver;
+    String wifiList[];
+    List<WifiData> wifiDataList;
+    ListView wifiListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiListView = (ListView) findViewById(R.id.wifiListView);
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiReceiver = new WifiScanReceiver();
+
+        wifiManager.startScan();
     }
+
+    @Override
+    protected void onResume(){
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        unregisterReceiver(wifiReceiver);
+        super.onPause();
+    }
+
+    private class WifiScanReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> scanResultList = wifiManager.getScanResults();
+            wifiList = new String[scanResultList.size()];
+            wifiDataList = new ArrayList<>();
+
+            for (ScanResult s : scanResultList) {
+                WifiData w = new WifiData(s);
+                wifiDataList.add(w);
+            }
+
+            for (int i = 0; i < scanResultList.size(); i++) {
+                //wifiList[i] = (scanResultList.get(i)).toString();
+                wifiList[i] = wifiDataList.get(i).toString();
+            }
+
+            ArrayAdapter<String> wifiAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, wifiList);
+            wifiListView.setAdapter(wifiAdapter);
+        }
+    }
+
 }
---
+++ app/src/main/java/dandev237/wifitest/WifiData.java
@@ -0,0 +1,65 @@
+package dandev237.wifitest;
+
+import android.net.wifi.ScanResult;
+import android.os.SystemClock;
+
+import java.text.DecimalFormat;
+import java.util.Date;
+
+/**
+ * Clase para modelar toda la información de una red wifi (Objeto ScanResult)
+ * <p/>
+ * Autor: Daniel Castro García
+ * Email: dandev237@gmail.com
+ * Fecha: 14/05/2016
+ */
+public class WifiData {
+
+    String bssid, ssid, capabilities;
+    int centerFreq0, centerFreq1, channelWidth, frequency, level;
+    Date timestamp;
+    String distance;
+
+    public WifiData(ScanResult scan){
+        bssid = scan.BSSID;
+        ssid = scan.SSID;
+        capabilities = scan.capabilities;
+        //centerFreq0 = scan.centerFreq0; //Centro de frecuencia del primer segmento
+        //centerFreq1 = scan.centerFreq1; //Centro de frecuencia del segundo segmento
+        //channelWidth = scan.channelWidth;
+        frequency = scan.frequency;
+        level = scan.level;
+
+        long scanResultTimestampInMillis = System.currentTimeMillis() - SystemClock.elapsedRealtime() + (scan.timestamp / 1000);
+        timestamp = new Date(scanResultTimestampInMillis);
+
+        /*DecimalFormat df = new DecimalFormat("#.##");
+        distance = df.format(trilaterate(level, frequency));*/
+    }
+
+
+    /*
+     * Trilateración para determinar la distancia al AP/dispositivo a partir del
+     * nivel de la señal.
+     *
+     * Para esto se ha empleado la fórmula para el cálculo del Free-space path loss (FSPL)
+     * Referencia:
+     *
+     * @param level Nivel de la señal, en dB.
+     * @param freq  Frecuencia de la señal, en MHz
+     */
+
+    /*
+    public double trilaterate(double level, double freq){
+        double exp = (27.55 - (20 * Math.log10(freq)) + Math.abs(level)) / 20.0;
+        return Math.pow(10.0, exp);
+    }
+    */
+
+    @Override
+    public String toString(){
+        return "BSSID: " + bssid + ", SSID: " + ssid + ", Capabilities: " + capabilities + /*", centerFreq0: " + centerFreq0 + ", centerFreq1: " + centerFreq1 +
+                ", Channel Width: " + channelWidth + */", Frequency: " + frequency + ", Signal level: " + level + ", Timestamp: " + timestamp /*". Distance: " +
+                distance + "."*/;
+    }
+}
